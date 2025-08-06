43b9c4787530c34218cf596dedffe65811c90d12
Basim-B-1998 <basimbangalath8075@gmail.com>
2025-08-06 19:40:54 +0530

    polish

--- frontend/components/buttons/SecondaryButton.tsx
+++ frontend/components/buttons/SecondaryButton.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export const SecondaryButton = ({children,onClick,size="small"} : {children: ReactNode,onClick: () => void, size?: "big" | "small"}) => {
  
-  return <div onClick={onClick} className={` border rounded-sm font-medium hover:shadow-md cursor-pointer text-black ${size === "small" ? "text-sm px-4 py-3 mr-4 mb-2 mt-2" : "text-xl px-8 py-2"}`}>
+  return <div onClick={onClick} className={` border rounded-sm font-medium  cursor-pointer text-black ${size === "small" ? "text-sm px-4 py-3 mr-4 mb-2 mt-2" : "text-xl px-8 py-2"}border-gray-300 hover:shadow-md`}>
     {children}
   </div>
 }
