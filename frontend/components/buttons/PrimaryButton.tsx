9fe15a0aefdc1ba680e19e8678e666dd8c6648bd
Basim-B-1998 <basimbangalath8075@gmail.com>
2025-08-06 19:30:47 +0530

    polish

--- frontend/components/buttons/PrimaryButton.tsx
+++ frontend/components/buttons/PrimaryButton.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 export const PrimaryButton = ({children,onClick,size="small"} : {children: ReactNode,onClick: () => void, size?: "big" | "small"}) => {
  
-  return <div onClick={onClick} className={` bg-amber-600 rounded-sm hover:shadow-md cursor-pointer text-center text-white ${size === "small" ? "text-sm px-4 py-3 mr-4 mb-2 mt-2" : "text-xl px-8 py-2"}`}>
+  return <div onClick={onClick} className={` bg-amber-600 rounded-sm hover:bg-amber-700 hover:shadow-md cursor-pointer text-center text-white ${size === "small" ? "text-sm px-4 py-3 mr-4 mb-2 mt-2" : "text-xl px-8 py-2"}`}>
     {children}
   </div>
 }
