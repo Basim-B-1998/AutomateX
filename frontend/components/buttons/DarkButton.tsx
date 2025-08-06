a81070c6708c1a62ea8f4969858e4f7f045ca547
Basim-B-1998 <basimbangalath8075@gmail.com>
2025-08-06 19:44:40 +0530

    polish

--- frontend/components/buttons/DarkButton.tsx
+++ frontend/components/buttons/DarkButton.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 export const DarkButton = ({children,onClick,size="small"} : {children: ReactNode,onClick: () => void, size?: "big" | "small"}) => {
  
-  return <div onClick={onClick} className={`flex flex-col justify-center px-8 py-2 bg-purple-800 rounded hover:shadow-md cursor-pointer text-center text-white `}>
+  return <div onClick={onClick} className={`flex flex-col justify-center px-8 py-2 bg-purple-800 hover:bg-purple-900 rounded hover:shadow-md cursor-pointer text-center text-white `}>
     {children}
   </div>
 }
